3f792e4f0ee59c1a11999bedb01ff80b3f3d91ba
bhavankumar2911 <bhavankumarcse2020@gmail.com>
2022-12-26 20:51:12 +0530

    initial commit

---
+++ components/About.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Name from "./Name";
+
+const About = ({ align }) => {
+  return (
+    <div className={`section-card md:m-0 text-${align}`}>
+      <div className="hidden md:block">
+        <Name />
+      </div>
+      <div className="md:hidden">
+        <h2 className="section-title">About</h2>
+      </div>
+      <p className="section-text md:mb-5">
+        I'm a self-taught full-stack web developer specializing in the MERN
+        stack. I write optimized code that makes the web app faster and more
+        scalable.
+      </p>
+    </div>
+  );
+};
+
+export default About;
